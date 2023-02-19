721f94e391c6de36a7bccec4b3495a51171e82b1
samuel <cpt.macmilan24@gmail.com>
2023-02-19 12:57:02 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 58; n++)
-		putchar(n);
-
+	for (n = 48; n < 58; n++)
+	
+	
 	if (n != 57)
 	{
 		putchar(',');
 		putchar(' ');
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
